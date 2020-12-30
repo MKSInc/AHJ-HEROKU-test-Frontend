feee7d0a52862ee21370af8a84d040a5dea73b9d
Maksim <pyshkov.maksim@gmail.com>
2020-12-30 15:59:23 +0200

    feat: testing...

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,25 @@
-// eslint-disable-next-line no-console
+/* eslint-disable no-console */
 console.log('main.js run');
+
+function sendQuery(url) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('PUT', url);
+
+  xhr.addEventListener('load', () => {
+    console.log('Loaded:', url);
+    console.log('Response:', xhr);
+  });
+
+  xhr.addEventListener('error', () => {
+    console.log('error', xhr);
+  });
+
+  xhr.send();
+}
+
+// sendQuery('http://localhost:7070/about');
+
+sendQuery('https://heroku-server-node-js-test.herokuapp.com');
+
+sendQuery('https://heroku-server-koa-test.herokuapp.com');
